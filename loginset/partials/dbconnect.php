c5f94db80edf30d52dcebf519357c7d3999e4574
pranjali9590 <112328612+pranjali9590@users.noreply.github.com>
2022-12-09 22:05:13 +0530

    Add files via upload

---
+++ loginset/partials/dbconnect.php
@@ -0,0 +1,12 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "user";
+
+$conn = mysqli_connect($server, $username, $password, $database);
+if(!$conn){
+    echo"SUCCESS";
+}
+?>
